e603fc530f504e94bf322ebf56f83df073773c2a
3rho <alexandre.everberg@gmail.com>
2024-03-07 15:42:56 +0100

    here

--- hw1.py
+++ hw1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
 
 
 def parameters():
@@ -11,8 +12,11 @@ def parameters():
   dynamic_pressure = []
   stagnation_dynamic_pressure = 137.2
   rho = 1.2112
+  theta = np.arange(0,190,10)
+  theta_stagnation_point = np.arange(-10,15,5)
+  dynamic_pressure_around_stagnation_point = np.array([116.0, 128.5, 137.2, 140.4, 137.3])
 
-  return hung_mass, tension_UD, C_De, g, U, dynamic_pressure, stagnation_dynamic_pressure, rho
+  return hung_mass, tension_UD, C_De, g, U, dynamic_pressure, stagnation_dynamic_pressure, rho, theta, theta_stagnation_point, dynamic_pressure_around_stagnation_point
 
 
 def regression_calibration_data(hung_mass, tension_UD, g, display):
@@ -47,16 +51,38 @@ def drag_computation(k_D, U, tension_UD):
 
 def U_infinity_computation(stagnation_dynamic_pressure, rho):
   U_infinity = (2 * stagnation_dynamic_pressure/ rho)**.5
-  print(f'U_infty = {U_infinity}')
+  print(f'U_infty = {U_infinity:.4f} [m/s]')
 
+def interpolation(theta, dyn_pressure):
+
+  # Compute the Lagrange interpolating polynomial
+  poly = lagrange(theta_stagnation_point, dynamic_pressure_around_stagnation_point)
+
+  # Find the maximum value and corresponding x-value
+  max_value = np.max(dynamic_pressure_around_stagnation_point)
+  max_x = theta_stagnation_point[np.argmax(dynamic_pressure_around_stagnation_point)]
+
+  print(f"Maximum value occurs at x = {max_x:.3f}, y = {max_value:.3f}.")
+
+  # Plot the Lagrange polynomial
+  x_new = np.linspace(min(theta_stagnation_point) - 4, max(theta_stagnation_point) + 4, 101)
+  plt.scatter(theta_stagnation_point, dynamic_pressure_around_stagnation_point, label='Data')
+  plt.plot(x_new, poly(x_new), label='Lagrange Polynomial', color='red')
+  plt.plot(max_x, max_value, 'rx', markersize=10, label=f'Maximum Point (theta = {max_x}[°], Dynamic Pressure = {max_value} [Pa])')  # Mark maximum point
+  plt.xlabel('Theta [°]')
+  plt.ylabel('Dynamic Pressure [Pa]')
+  plt.title('Lagrange Interpolation of the stagnation point')
+  plt.legend()
+  plt.grid()
+  plt.savefig('stagnation_point_lagrange_interpolation', transparent = True)
+  plt.show()
 
 if __name__ == '__main__':
-  hung_mass, tension_UD, C_De, g, U, dynamic_pressure, stagnation_dynamic_pressure, rho = parameters()
+  hung_mass, tension_UD, C_De, g, U, dynamic_pressure, stagnation_dynamic_pressure, rho, theta, theta_stagnation_point, dynamic_pressure_around_stagnation_point = parameters()
   k_D = regression_calibration_data(hung_mass, tension_UD, g, display = False)
   F_D = drag_computation(k_D, U, tension_UD)
-
   U_infinity = U_infinity_computation(stagnation_dynamic_pressure, rho)
-
+  interpolation(theta_stagnation_point, dynamic_pressure_around_stagnation_point)
 
 
 
